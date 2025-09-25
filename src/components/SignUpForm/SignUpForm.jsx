a8e3fbc66973c9dfbe918be89df4326363b1a077
melabreu <melissa.abreu84@gmail.com>
2025-09-25 10:53:49 -0400

    setup sign up form

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+
+const SignUpForm = () => {
+    const navigate = useNavigate();
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        passwordConf: '',
+    });
+
+    const { username, password, passwordConf } = formData;
+
+    const handleChange = (event) => {
+        setMessage('');
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formData);
+    }
+
+    const isFormInvalid = () => {
+        return !(username && password && password === passwordConf);
+    }
+
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='username'>Username:</label>
+                    <input 
+                        type='text'
+                        id='name'
+                        value={username}
+                        name='username'
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label htmlFor='password'>Password:</label>
+                    <input 
+                        type='password'
+                        id='password'
+                        value={password}
+                        name='password'
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label htmlFor='confirm'>Confirm Password:</label>
+                    <input 
+                        type='password'
+                        id='confirm'
+                        value={passwordConf}
+                        name='passwordConf'
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <button disabled={isFormInvalid()}>Sign Up</button>
+                    <button onClick={() => navigate('/')}>Calcel</button>
+                </div>
+            </form>
+        </main>
+    );
+}
+
+export default SignUpForm;
