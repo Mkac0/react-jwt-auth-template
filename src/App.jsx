5ab8c33bc710be257c0ef64f00d1deb61f33c3a3
melabreu <melissa.abreu84@gmail.com>
2025-09-25 10:25:33 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+const App = () => {
+  
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, Mel!</h1>
+    </>
+  );
+};
+
+export default App;
