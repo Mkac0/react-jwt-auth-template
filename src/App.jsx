77e914b51a5a118e682286b9d48477cc12e4db2a
melabreu <melissa.abreu84@gmail.com>
2025-09-25 13:12:14 -0400

    setup dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useContext } from 'react';
 import { Routes, Route } from 'react-router';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,13 +8,19 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import SignUpForm from './components/SignUpForm/SignUpForm'
 import SignInForm from './components/SignInForm/SignInForm';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+
+import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+  const { user } = useContext(UserContext);
   
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
       </Routes>
